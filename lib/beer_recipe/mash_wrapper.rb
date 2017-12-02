5649bf9431b849a4148bed7b42b7dcd006d1ba20
Olle Johansson <olle@avidity.se>
2017-12-02 16:16:11 +0100

    Handle empty mash steps.

--- lib/beer_recipe/mash_wrapper.rb
+++ lib/beer_recipe/mash_wrapper.rb
@@ -1,12 +1,11 @@
 class BeerRecipe::MashWrapper < BeerRecipe::Wrapper
-
   def initialize(record, recipe=nil)
     @record = record
     @recipe = recipe
   end
 
   def steps
-    return [] if @record.nil?
+    return [] if @record.nil? || @record.mash_steps.nil? || @record.mash_steps.empty?
     @steps ||= @record.mash_steps.map do |step|
       BeerRecipe::Wrapper.wrap(step, @recipe)
     end
